2fa0f90add9344801b67f67cd72f364ea8eb8788
Riccardo De Cesaris <riccardo.dcesaris@gmail.com>
2024-03-26 14:07:50 +0100

    pippo dal ginocchio

---
+++ Giorgione/Database.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Davide Pierotti <d.pierotti@live.it>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Giorgione.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Giorgione;
+internal class Database 
+{
+    public DbSet<User> Users { get; set; }
+}
