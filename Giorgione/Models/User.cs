cef7e61bb32249b3cb7ad9b3ef9359968b1dd888
Riccardo De Cesaris <riccardo.dcesaris@gmail.com>
2024-03-26 14:07:50 +0100

    pippo dal ginocchio

---
+++ Giorgione/Models/User.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Davide Pierotti <d.pierotti@live.it>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giorgione.Models;
+internal class User
+{
+    [Key]
+    public ulong UserId { get; set; }
+
+    public DateOnly Birthday { get; set; }
+}
