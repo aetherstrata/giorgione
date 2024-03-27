c0ea58b4c255a3780367429d82d461ed94cff6be
Izurby <132925525+Izurby@users.noreply.github.com>
2024-03-27 21:10:34 +0100

    Add birthdate command stub

---
+++ Giorgione/Modules/Birthdate.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Davide Pierotti <d.pierotti@live.it>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using Discord.Interactions;
+using Discord;
+
+namespace Giorgione.Modules;
+
+public class Birthdate : InteractionModuleBase<SocketInteractionContext>
+{
+    [SlashCommand("birthday", "Auguri di compleanno")]
+    public Task BirthDayAsync()
+    {
+        var embed = new EmbedBuilder()
+            .WithColor(Color.Blue)
+            .WithAuthor(a => a.WithName("\uD83C\uDF82 Buon Compleanno!"))
+            .WithDescription(
+                $"""
+                 Sono Davvero Contento che oggi sia il compleanno di <@{Context.Guild.Id}>                 
+                 """)
+            .Build();
+
+        return RespondAsync(embed: embed);
+    }
+}
