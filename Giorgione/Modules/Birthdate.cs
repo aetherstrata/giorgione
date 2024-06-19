0d1d95079d7efbfe4d01e72d43f258686b418bcb
Izurby <132925525+Izurby@users.noreply.github.com>
2024-06-20 01:02:10 +0200

    Add list command and user parameter to show command (#6)
    * add parameter on show command + list command
    
    * Minor fixes
    
    * Rename user parameter
    
    ---------
    
    Co-authored-by: Davide Pierotti <d.pierotti@live.it>

--- Giorgione/Modules/Birthdate.cs
+++ Giorgione/Modules/Birthdate.cs
@@ -20,20 +20,15 @@ public class Birthdate(
     ILogger<Birthdate> logger) : InteractionModuleBase<SocketInteractionContext>
 {
     [SlashCommand("show", "Show your birthdate")]
-    public Task GetBirthdayAsync()
+    public Task GetBirthdayAsync(IUser? user = null)
     {
         try
         {
-            User? user;
+            using var db = dbFactory.CreateDbContext();
 
-            using (var db = dbFactory.CreateDbContext())
-            {
-                user = db.Find<User>(Context.User.Id);
-            }
+            var userEntity = db.Users.Find(user?.Id ?? Context.User.Id);
 
-            return user is null
-                ? RespondAsync($"Not set")
-                : RespondAsync($"{user.Birthday}");
+            return RespondAsync(userEntity is null ? "Not set" : $"{userEntity.Birthday}");
         }
         catch (Exception e)
         {
@@ -49,7 +44,7 @@ public class Birthdate(
         {
             User? user;
 
-            var date = DateOnly.ParseExact(birthday, ["yyyy-M-d", "M-d"], CultureInfo.InvariantCulture);
+            var date = DateOnly.ParseExact(birthday, ["yyyy-M-d", "M-d", "d-M-yyyy", "d/M", "yyyy/M/d", "d/M/yyyy"], CultureInfo.InvariantCulture);
 
             using (var db = dbFactory.CreateDbContext())
             {
@@ -73,7 +68,8 @@ public class Birthdate(
             }
 
             var embed = new EmbedBuilder()
-                .WithDescription($"{user.Birthday}")
+                .WithColor(Color.Teal)
+                .WithDescription($"Your birthday has been set to {user.Birthday.Value.Day}/{user.Birthday.Value.Month}")
                 .Build();
 
             return RespondAsync(embed: embed);
@@ -85,7 +81,7 @@ public class Birthdate(
             var formatEmbed = new EmbedBuilder()
                 .WithColor(Color.Red)
                 .WithTitle("Format error")
-                .WithDescription("The birthdate does not have a valid format.\nValid formats are: `YYYY-M-D` and `M-D`")
+                .WithDescription("The birthdate does not have a valid format.\nValid formats are:\n- `YYYY-M-D` | `M-D` | `D-M-YYYY`\n- `YYYY/M/D` | `M/D` | `D/M/YYYY`")
                 .Build();
 
             return RespondAsync(embed: formatEmbed);
@@ -94,6 +90,33 @@ public class Birthdate(
         {
             logger.LogError(e, "An error occurred while processing a '/birthday set' command");
             return RespondAsync("An error occurred", ephemeral: true);
+        }
+    }
+
+    [SlashCommand("list", "List all birthday defined in this server")]
+    public async Task BirthdayListAsync()
+    {
+        try
+        {
+            await using var db = await dbFactory.CreateDbContextAsync();
+
+            var list = await db.Users
+                .Where(user => user.Birthday.HasValue)
+                .Select(user => $"<@{user.Id}> - {user.Birthday}")
+                .ToListAsync();
+
+            var listEmbed = new EmbedBuilder()
+                .WithColor(Color.Blue)
+                .WithTitle("Lista dei festeggiati")
+                .WithDescription(string.Join('\n',list))
+                .Build();
+
+            await RespondAsync(embed: listEmbed);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "An error occurred while processing a '/birthday list' command");
+            await RespondAsync("An error occurred", ephemeral: true);
         }
     }
 }
