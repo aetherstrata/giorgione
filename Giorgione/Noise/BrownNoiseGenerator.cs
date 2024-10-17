9e3d56798bf4d660ebe066b8ec7ad753a45e2915
Davide Pierotti <d.pierotti@live.it>
2024-10-17 22:44:04 +0200

    Revert tweak

--- Giorgione/Noise/BrownNoiseGenerator.cs
+++ Giorgione/Noise/BrownNoiseGenerator.cs
@@ -5,25 +5,22 @@ namespace Giorgione.Noise;
 
 public class BrownNoiseGenerator(int sampleRate, int seconds) : NoiseGenerator(sampleRate, seconds)
 {
-    private const double beta = 0.025;
+    private const double beta = 0.022;
 
     /// <inheritdoc />
     public override int Generate(double amplitude)
     {
-        for (int time = 0; time < Duration; time++)
+        double sample = Tick();
+
+        for (int i = 0; i < Buffer.Length / 2; i++)
         {
-            double sample = Tick();
+            sample -= beta * (sample - Tick());
 
-            for (int i = 0; i < Buffer.Length / 2; i++)
-            {
-                sample -= beta * (sample - Tick());
+            short pcmValue = (short)(sample * amplitude * short.MaxValue);
 
-                short pcmValue = (short)(sample * amplitude * short.MaxValue);
-
-                // Convert the sample to little-endian
-                Buffer.Span[time * SampleRate + i * 2] = (byte)(pcmValue & 0xFF);
-                Buffer.Span[time * SampleRate + i * 2 + 1] = (byte)((pcmValue >> 8) & 0xFF);
-            }
+            // Convert the sample to little-endian
+            Buffer.Span[i * 2] = (byte)(pcmValue & 0xFF);
+            Buffer.Span[i * 2 + 1] = (byte)((pcmValue >> 8) & 0xFF);
         }
 
         return Buffer.Length;
