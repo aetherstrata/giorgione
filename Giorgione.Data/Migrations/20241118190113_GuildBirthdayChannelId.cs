b92c578ed30bb64f5f71125e7e3a0736d380d5c8
Davide Pierotti <d.pierotti@live.it>
2024-11-18 20:02:29 +0100

    Add birthday channel id to guild model

---
+++ Giorgione.Data/Migrations/20241118190113_GuildBirthdayChannelId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Giorgione.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class GuildBirthdayChannelId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "birthday_channel_id",
+                table: "guilds",
+                type: "numeric(20,0)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "birthday_channel_id",
+                table: "guilds");
+        }
+    }
+}
